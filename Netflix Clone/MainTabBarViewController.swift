00be2b11bfe53a51535ed2ff271d2a1e59ccdc6b
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-06-26 14:16:35 +0600

    Initial Commit

---
+++ Netflix Clone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Shahriar Ahsan Taisiq on 26/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
